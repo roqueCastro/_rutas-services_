2ddc0c6aafea086b73b1bf93615b7ab1f6a40ce4
alien <rokke1558@gmail.com>
2019-11-22 12:08:38 -0500

    update adms

---
+++ _ws_nn_code_update_adm_.php
@@ -0,0 +1,93 @@
+<html>
+    <head>
+        <script crossorigin="anonymous" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" src="https://code.jquery.com/jquery-3.3.1.min.js">
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+			    cargardates();
+			});
+
+			function cargardates(){
+				 $.ajax({
+			        url: 'procesar.php',
+			        data: {
+			            Operacion: 'service'
+			           
+			        },
+			        type: 'POST',
+			        dataType: 'json',
+			        beforeSend: function() {
+			            //alert('Procesando informacion..');
+			            
+			        },
+			        success: function(datos) {
+
+			            for (var i=0; i<datos.length; i++){
+			            	$('#name').val(datos[i].nombre_administrador);
+			            	$('#user').val(datos[i].userr);
+				            $('#pass').val(datos[i].pass);
+				            $('#tele').val(datos[i].telefono);
+				            $('#lat').val(datos[i].latitud);
+				            $('#lng').val(datos[i].longitud);
+			            }
+			        },
+			        error: function(xhr, status) {}
+			    });
+			}
+
+            function updateadm(){
+            /*	alert( $('#name').val()+
+			          $('#user').val()+
+			           $('#pass').val()+
+			           $('#tele').val()+
+			           $('#lat').val()+
+			          $('#lng').val());*/
+			          
+             	 $.ajax({
+			        url: 'procesar.php',
+			        data: {
+			            Operacion: 'update',
+			            name_adm : $('#name').val(),
+			            user : $('#user').val(),
+			            pass : $('#pass').val(),
+			            tele : $('#tele').val(),
+			            lat : $('#lat').val(),
+			            lng : $('#lng').val()
+			        },
+			        type: 'POST',
+			        dataType: 'html',
+			        beforeSend: function() {
+			            //alert('Procesando informacion..');
+			            
+			        },
+			        success: function(datos) {
+			            alert(datos);
+			        },
+			        error: function(xhr, status) {}
+			    });
+             }
+        </script>
+    </head>
+    <body>
+        <h1>
+            Actualizar adm
+        </h1>
+        <form>
+            nombre_administrador:
+            <input id="name" type="text"/>
+            user
+            <input id="user" type="text" value=""/>
+            pass:
+            <input id="pass" type="password" value=""/>
+            telefono
+            <input id="tele" type="text" value=""/>
+            latitud
+            <input id="lat" type="text"/>
+            longitud
+            <input id="lng" type="text"/>
+            <button id="envio" onclick="updateadm()">
+                Actializar
+            </button>
+        </form>
+    </body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,71 @@
+<?php
+
+$operacion = $_POST['Operacion'];
+//$operacion = "info_factura";
+
+if ($operacion == "update") {
+
+    $name = $_POST['name_adm'];
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    $tele = $_POST['tele'];
+    $lat  = $_POST['lat'];
+    $lng  = $_POST['lng'];
+
+    require_once '_conexion_/_conexion.php';
+
+    try {
+
+        $base->beginTransaction();
+
+        $sql    = "UPDATE colegio SET nombre_administrador = ?, userr = ?, pass = ?, telefono = ?, latitud = ?, longitud = ?   WHERE id_colegio = 1 ";
+        $result = $base->prepare($sql);
+
+        $result->bindParam(1, $name, PDO::PARAM_STR);
+        $result->bindParam(2, $user, PDO::PARAM_STR);
+        $result->bindParam(3, $pass, PDO::PARAM_STR);
+        $result->bindParam(4, $tele, PDO::PARAM_STR);
+        $result->bindParam(5, $lat, PDO::PARAM_STR);
+        $result->bindParam(6, $lng, PDO::PARAM_STR);
+
+        if ($result->execute()) {
+            echo 'registro';
+        } else {
+            echo "00";
+        }
+        //
+        $base->commit();
+    } catch (Exception $e) {
+        $base->rollback();
+        echo '000';
+    }
+
+    // echo $name . "\n" . $user . "\n" . $pass . "\n" . $tele . "\n" . $lat . "\n" . $lng;
+    // echo 'holaproceso';
+}
+if ($operacion == "service") {
+
+    require_once '_conexion_/_conexion.php';
+
+    try {
+
+        $base->beginTransaction();
+
+        $sql    = "SELECT * FROM colegio";
+        $result = $base->prepare($sql);
+        if ($result->execute()) {
+            $results = $result->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($results);
+        } else {
+            echo "00";
+        }
+        //
+        $base->commit();
+    } catch (Exception $e) {
+        $base->rollback();
+        echo '000';
+    }
+
+    // echo $name . "\n" . $user . "\n" . $pass . "\n" . $tele . "\n" . $lat . "\n" . $lng;
+    // echo 'holaproceso';
+}
